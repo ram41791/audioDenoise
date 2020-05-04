abdab9a0bf6dbcd29c0801813815d0d1a55b78d7
Varun Nair <nairv@purdue.edu>
2020-05-04 18:26:21 -0400

    algo for simple waveforms

--- PyFiles/fouriering.py
+++ PyFiles/fouriering.py
@@ -5,6 +5,10 @@ threshold in result in the frequency domain -> convert the audio file vector bac
 import numpy as np
 import scipy as sp
 from scipy.io import wavfile
+from scipy.signal import convolve
+import pywt
+import math
+import matplotlib.pyplot as plt
 import wavio
 import pyaudio
 import audio2numpy
@@ -18,6 +22,25 @@ def readAudioFileAsVector(filepath):
     r, d = sp.io.wavfile.read(filepath)
     return r, d
 
+def Extract(d,chanNo):
+    """
+    This function extracts sound data from chosen channel
+    :param d: data
+    :param chanNo: channel number
+    :return: chanel data
+    """
+    return [item[chanNo] for item in d]
+
+def sineWaveGen():
+     time = np.arange(0,48*math.pi, math.pi/1000)
+     timeplot = time*(8)
+     amplitude = np.sin(timeplot)+np.sin(3*timeplot)+np.cos(5*timeplot)+np.cos(timeplot)+np.sin(timeplot*8)
+
+     plt.plot(time, amplitude)
+     plt.show()
+     return(amplitude)
+
+
 def writeVectorAsAudio(r, d):
     """
     This function will convert a vetor into a playable audio file
@@ -25,7 +48,7 @@ def writeVectorAsAudio(r, d):
     :param d: audio file as vector
     :return: nil
     """
-    sp.io.wavfile.write("recreatedAudio.wav", r, d)
+    sp.io.wavfile.write("cleanedSine.wav", r, d)
 
 
 def FourierTransformOfSignal(data):
@@ -37,28 +60,46 @@ def FourierTransformOfSignal(data):
     AudioInFrequencyDomain = np.fft.rfft(data)
     return AudioInFrequencyDomain
 
+def noiseConvolve(cleanFile):
+    noiseFilter = np.random.normal(0, .1, cleanFile.shape)
+    returnVal = cleanFile+noiseFilter
+    return returnVal
+
 def thresholdingInFrequencyDomain(fftAudio, threshold):
     """
     This function will threshold a frequency domain signal
     :param fftAudio: signal in frequency domain
     :return: thresholded signal
     """
-    result = (fftAudio < threshold) * fftAudio
+    result = pywt.threshold(fftAudio, threshold, "soft")
+    # result = np.fft.fftshift(intermediateResult)
     return result
+
+def wienerFiltering(inputSignal):
+    Wfiltered = sp.signal.wiener(inputSignal)
+    return  Wfiltered
+
 
 def FourierInverted(signal):
     """
     This function will invert the FFT and return a writeable vector
     :param signal: thresholded
-    :return:
+    :return: signal in time domain (inverse of the fourier transform)
     """
     inverseFFT = np.fft.irfft(signal)
     return inverseFFT
 
+
+
 if __name__ == "__main__":
     fileToTest = "/Users/varunnair/Desktop/AudioDNS/gitFolder/audioDenoise/PyFiles/NewRecording.wav"
     rate, data = readAudioFileAsVector(fileToTest)
-    ftData = FourierTransformOfSignal(data)
-    thresholdedSignal = thresholdingInFrequencyDomain(ftData, 90)
+    chanelData =  sineWaveGen() #Extract(data, 0)
+    noisyFile = noiseConvolve(np.asarray(chanelData))
+    ftData = FourierTransformOfSignal(noisyFile)
+    thresholdedSignal = thresholdingInFrequencyDomain(ftData, 9600)
+    wienerFiltered = wienerFiltering(ftData)
     invertedPostThreshold = FourierInverted(thresholdedSignal)
-    writeVectorAsAudio(rate, invertedPostThreshold)
+    print(chanelData-invertedPostThreshold)
+    writeVectorAsAudio(48000, invertedPostThreshold)
+    print("done")
